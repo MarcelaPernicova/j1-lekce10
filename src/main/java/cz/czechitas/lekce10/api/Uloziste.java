d6c98469084f8406f49ea7c6c820645ef8cb1ceb
vojtechbastl <Bastl.vojtech@gmail.com>
2023-04-18 20:35:58 +0200

    zmeny

--- src/main/java/cz/czechitas/lekce10/api/Uloziste.java
+++ src/main/java/cz/czechitas/lekce10/api/Uloziste.java
@@ -16,6 +16,7 @@ public class Uloziste {
 
     public void nacistPlochuZeSouboru(Path file) throws IOException {
         UlozenaPlocha ulozenaPlocha = objectMapper.readValue(file.toFile(), UlozenaPlocha.class);
+
         cat = new Cat(ulozenaPlocha.getCat());
         mouse = new Mouse(ulozenaPlocha.getMouse());
         for (Point treePoint : ulozenaPlocha.getTrees()) {
@@ -27,13 +28,28 @@ public class Uloziste {
 
     public void nacistPlochuZeSouboru() throws IOException {
         nacistPlochuZeSouboru(Paths.get("level-01.json"));
+
+
+
     }
 
     public void nacistStavZeSouboru(Path path)  throws IOException {
         // TODO
+
         // Načíst objekt UlozenyStav pomocí objectMapper.readValue(file, UlozenyStav.class)
+         UlozenyStav ulozenyStav = objectMapper.readValue(path.toFile(), UlozenyStav.class);
         // Získat z UlozenyStav souřadnice kočky a myši
+
+
+        Point nacistStavKocky = ulozenyStav.getCat();
+        Point nacistStavMysi = ulozenyStav.getMouse();
+
+
+
         // Zapsat tyto souřadnice do objektů kočky a myši pomocí setLocation()
+        cat.setLocation(nacistStavKocky);
+        mouse.setLocation(nacistStavMysi);
+
     }
 
     public void nacistStavZeSouboru() throws IOException {
@@ -43,8 +59,15 @@ public class Uloziste {
     public void ulozitStavDoSouboru(Path path)  throws IOException {
         // TODO
         // Vytvořit objekt UlozenyStav
+        UlozenyStav ulozenyStav = new UlozenyStav();
         // Uložit do něj souřadnice kočky a myši – souřadnice získáte voláním getLocation()
+        Point souradniceKocky = cat.getLocation();
+        Point souradniceMysi = mouse.getLocation();
+
+        ulozenyStav.setCat(souradniceKocky);
+        ulozenyStav.setMouse(souradniceMysi);
         // Uložit objekt UlozenyStav do souboru pomocí objectMapper.writeValue(file, object)
+        objectMapper.writeValue(path.toFile(), ulozenyStav);
     }
 
     public void ulozitStavDoSouboru() throws IOException {
